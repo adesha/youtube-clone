229d65e982a7407ffa78e037ffeea8f8eb372201
adesha <adeshaholagodu@gmail.com>
2023-11-22 21:04:22 +0530

    video details completed

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -4,6 +4,7 @@ import { CheckCircle } from '@mui/icons-material'
 import { demoChannelTitle,demoChannelUrl,demoThumbnailUrl,demoVideoTitle,demoVideoUrl } from '../utilities/constants'
 
 function VideoCard({video:{id:{videoId},snippet}}) {
+  console.log(snippet)
   return (
     <Card sx={{width:{md:'320px',xs:'100%'},
     boxShadow:'none',borderRadius:0}}>
@@ -15,12 +16,12 @@ function VideoCard({video:{id:{videoId},snippet}}) {
         <CardContent sx={{backgroundColor:'#1e1e1e',height:'160px'}}>
           <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
             <Typography variant='subtitle1' fontWeight='bold' color='#FFF'>
-              {snippet?.title.slice(0,60)}||{demoVideoTitle.slice(0,60)}
+              {snippet?.title.slice(0,60)||demoVideoTitle.slice(0,60)}
             </Typography>
           </Link>
           <Link to={snippet?.channelId?`/channel/${snippet?.channelId}`:demoChannelUrl}>
             <Typography variant='subtitle2' fontWeight='bold' color='gray'>
-              {snippet?.channelTitle.slice(0,60)}||{demoChannelTitle.slice(0,60)}
+              {snippet?.channelTitle.slice(0,60) || demoChannelTitle.slice(0,60)}
               <CheckCircle sx={{fontSize:12, color:'gray',ml:'5px'}}/>
             </Typography>
           </Link>
